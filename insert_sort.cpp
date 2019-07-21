4b344348522f75e843549d6c545d8d6c86bcb706
koralle <mkhd7930@gmail.com>
2019-07-21 12:57:06 +0900

    挿入ソートのプログラム

---
+++ insert_sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 挿入ソート
+void insertion_sort(vector<int> &v, int n){
+    // v[i]をソート済みの列v[0..i-1]に追加する
+    for(int i = 1; i < n; i++){
+        // 挿入したい値を保存
+        int key = v[i];
+
+        // v[i-1] からv[0]までさかのぼる
+        int j = i-1;
+        while(j >= 0) {
+            // さかのぼる途中で挿入する位置が確定
+            if(v[j] <= key) break;
+
+            // v[j]を一つ後ろにずらす
+            v[j+1] = v[j];
+            j--;
+        }
+        // 値を挿入
+        v[j+1] = key;
+    }
+}
+
+int main(){
+    int N;
+    cin >> N;
+    vector<int> vx(N, 0);
+    for(int i = 0; i < N; i++) cin >> vx[i];
+
+    insertion_sort(vx, N);
+    for(int i = 0; i< N; i++) cout << "vx[" << i << "] = " << vx[i] << endl;
+
+    return 0;
+}
