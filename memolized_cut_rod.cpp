b945d66ec54a0b6713d358a2708d60b9246d334a
koralle <mkhd7930@gmail.com>
2019-07-29 00:53:44 +0900

    トップダウン型DP

--- memolized_cut_rod.cpp
+++ memolized_cut_rod.cpp
@@ -3,6 +3,9 @@
 #include <cmath>
 using namespace std;
 
+// トップダウン型DP
+// e.g. vx = [1, 5, 8, 8, 9, 10, 17, 17, 20, 24, 30], n = 10
+// -> return 30
 int memolized_cut_rod(const vector<int> &v, const int n);
 int memolized_cut_rod_aux(const vector<int> &v, const int n, vector<int> &r);
 
@@ -14,19 +17,16 @@ int memolized_cut_rod(const vector<int> &v, const int n){
 int memolized_cut_rod_aux(const vector<int> &v, const int n, vector<int> &r){
     int q = 0;
     if(r[n] >= 0) return r[n];
-    if (n == 0) {
-        q = 0;
-    }
+
+    if (n == 0) q = 0;
     else{
         q = -1000000;
-        for(int i = 0; i < n; i++){
-            q = max(q, v[i] + memolized_cut_rod_aux(v, n-(i+1), r));
-        }
+        // 部分問題を計算
+        for(int i = 0; i < n; i++) q = max(q, v[i] + memolized_cut_rod_aux(v, n-(i+1), r));
     }
     r[n] = q;
     return q;
 }
-
 
 int main(){
 
